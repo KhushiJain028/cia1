378aee97626f0f87c5cbe0263f5b424dba6d3fb9
KhushiJain028 <156777828+KhushiJain028@users.noreply.github.com>
2024-01-16 19:50:03 +0530

    Create QueueArray.py

---
+++ QueueArray.py
@@ -0,0 +1,31 @@
+class QueueArray:
+    def __init__(self):
+        self.queue = []
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            return "Queue is empty"
+
+    def front(self):
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            return "Queue is empty"
+
+    def size(self):
+        return len(self.queue)
+
+queue = QueueArray()
+queue.enqueue('a')
+queue.enqueue('b')
+queue.enqueue('c')
+queue.dequeue()
+print(queue.size())
